09b6356bdf53b4146be27f28d22f4ec9e6c86ded
medram <mramouchy@gmail.com>
2023-05-10 17:01:59 +0100

    SectionLoading compoment can be centerlized!

--- src/components/SectionLoading.jsx
+++ src/components/SectionLoading.jsx
@@ -1,10 +1,10 @@
 
 export default function SectionLoading(props)
 {
-    let { title, spinnerClassName, className, ...rest } = props
+    let { title, spinnerClassName, className, center=false, ...rest } = props
 
     return (
-        <div className={["d-flex justify-content-center align-items-center p-5", className].join(" ")} {...rest}>
+        <div className={["d-flex justify-content-center align-items-center p-5", className, center && "w-100"].join(" ")} {...rest}>
             <span className={["spinner-border mx-2", spinnerClassName].join(" ")} role="status"></span>
             {title && <b>{title}</b>}
         </div>
