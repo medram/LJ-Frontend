8d9a0c378978994aa4f89b51883e0ece4375cafe
medram <mramouchy@gmail.com>
2023-05-15 21:44:02 +0100

    PasswordInput updated

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -10,7 +10,7 @@ export default function PasswordInput({ ...restProps })
     return (
         <div className="input-group mb-3">
             <input type={show ? "text" : "password" } className="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1" {...restProps} />
-            <span className="input-group-text" id="basic-addon1" style={{ cursor: "pointer" }} onClick={() => setShow(!show)}>
+            <span className="input-group-text text-bg-primary" id="basic-addon1" style={{ cursor: "pointer" }} onClick={() => setShow(!show)}>
                 <FontAwesomeIcon icon={show ? faEyeSlash : faEye} />
             </span>
         </div>
