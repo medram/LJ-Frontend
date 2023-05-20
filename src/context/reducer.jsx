3008541c85d7d981c7a42c6e0f399a81fae0938d
medram <mramouchy@gmail.com>
2023-05-20 16:30:51 +0100

    Store context updated!

--- src/context/reducer.jsx
+++ src/context/reducer.jsx
@@ -1,12 +1,7 @@
 export default function reducer(stats, action) {
     switch (action.type) {
-        case "TOGGLE_THEME":
-            let newTheme = (stats.theme === "dark") ? "light" : "dark"
-            return { ...stats, theme: newTheme }
-        case "IS_AUTHENTICATED":
-            return { ...stats, isAuthenticated: action.payload }
-        case "UPDATE_USER":
-            return {...stats, user: action.user }
+        case "OVERRIDE_STORE":
+            return {...stats, ...action.payload}
 
         default:
             break;
