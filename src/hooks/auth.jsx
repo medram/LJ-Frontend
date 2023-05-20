99f0f296e7c403e8323d6740fd3da301ed463c5c
medram <mramouchy@gmail.com>
2023-05-20 16:22:35 +0100

    improved Store!

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,20 +1,26 @@
-import { useEffect, useState } from "react"
-import { useLocalStorage } from "."
+import { useEffect } from "react"
+import { useLocalStorage, useLocalStore } from "."
 import { auth, logout } from "../api/auth"
-import { useStore } from "../context/StoreContext"
 
 
-export function useAuth()
-{
-    const { dispatch } = useStore()
-    const [ token, setToken ] = useLocalStorage("tk", "")
+export function useAuth() {
+    const [isAuthenticated, setAuthenticated] = useLocalStore("isAuth", false)
+    const [user, setUser] = useLocalStore("user", {})
+    const [token, setToken] = useLocalStorage("tk", "")
+
+    useEffect(() => {
+        if (token == "")
+        {
+            setAuthenticated(false)
+        }
+    }, [token])
 
     const Authenticate = async (email, password) => {
         let data = await auth(email, password)
-        if (!data?.errors)
-        {
-            dispatch({type: "UPDATE_USER", user: data.user })
-            dispatch({ type: "IS_AUTHENTICATED", payload: true })
+
+        if (!data?.errors) {
+            setUser(data.user)
+            setAuthenticated(true)
             setToken(data.token)
         }
 
@@ -25,19 +31,48 @@ export function useAuth()
         // to inform the database
         let data = await logout()
 
-        dispatch({ type: "UPDATE_USER", user: {} })
-        dispatch({ type: "IS_AUTHENTICATED", payload: false })
+        setUser({})
+        setAuthenticated(false)
         setToken("")
     }
 
-    return { Authenticate, Logout, token }
+    return { isAuthenticated, user, Authenticate, Logout, token }
 }
+
+// export function useAuth()
+// {
+//     const { dispatch } = useStore()
+//     const [ token, setToken ] = useLocalStorage("tk", "")
+
+//     const Authenticate = async (email, password) => {
+//         let data = await auth(email, password)
+//         if (!data?.errors)
+//         {
+//             dispatch({type: "UPDATE_USER", user: data.user })
+//             dispatch({ type: "IS_AUTHENTICATED", payload: true })
+//             setToken(data.token)
+//         }
+
+//         return data
+//     }
+
+//     const Logout = async () => {
+//         // to inform the database
+//         let data = await logout()
+
+//         dispatch({ type: "UPDATE_USER", user: {} })
+//         dispatch({ type: "IS_AUTHENTICATED", payload: false })
+//         setToken("")
+//     }
+
+//     return { Authenticate, Logout, token }
+// }
 
 
 export function useUser()
 {
-    const { isAuthenticated, user } = useStore()
+    const { isAuthenticated, user } = useAuth()
 
-    return { isAuthenticated, isAdmin: user.role == 1, user }
+    return { isAuthenticated, isAdmin: user?.role == 1, user }
 }
 
--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,30 +1,47 @@
 import { useContext, useEffect, useReducer, useState } from "react"
 import { useQuery } from "react-query"
 import { getAvailablePaymentMethods, getDashboardSettings, getPlans, getSettings } from "../api"
-import StoreContext from "../context/StoreContext"
+import StoreContext, { useStore } from "../context/StoreContext"
 import { getDashboardPlans } from "../api/admin"
 
 
-export function useLocalStorage(key, value = null) {
-    const [persistedValue, SetPersistedValue] = useState(() => {
+export function useLocalStorage(key, value = null ) {
+    const [persistedValue, setPersistedValue] = useState(() => {
         try {
-            //console.log("localStorage useState function called!")
             let storedValue = localStorage.getItem(key)
-            if (storedValue !== null)
+            if (storedValue != null)
                 return JSON.parse(storedValue)
         } catch (error) {
-            return value
+
         }
+
         return value
     })
 
     // Sync values to localStorage
-    useEffect(() => {
-        //console.log("Sync to localStorage!", persistedValue)
-        localStorage.setItem(key, JSON.stringify(persistedValue))
-    }, [persistedValue])
+    const setPersistedValueDecorator = (value) => {
+        localStorage.setItem(key, JSON.stringify(value))
+        return setPersistedValue(value)
+    }
 
-    return [persistedValue, SetPersistedValue]
+    return [persistedValue, setPersistedValueDecorator]
+}
+
+
+export function useLocalStore(key, defaultValue)
+{
+    let { [key]: currentValue, dispatch } = useStore()
+
+    if (currentValue === undefined)
+    {
+        currentValue = defaultValue
+    }
+
+    const dispatchDecorator = (newValue) => {
+        return dispatch({ type: "OVERRIDE_STORE", payload: { [key]: newValue} })
+    }
+
+    return [currentValue, dispatchDecorator]
 }
 
 
@@ -85,7 +102,7 @@ export function useDashboardPlan(planId)
 
 export function usePlans()
 {
-    const { data, ...rest } = useQuery("plans", getPlans, { staleTime: 1000 * 60 })
+    const { data, ...rest } = useQuery("plans", getPlans, { staleTime: 1000 * 60 }) // 1 minute
     const plans = data?.plans
     const monthlyPlans = plans?.filter(plan => plan.billing_cycle === "monthly")
     const yearlyPlans = plans?.filter(plan => plan.billing_cycle === "yearly")
@@ -94,18 +111,9 @@ export function usePlans()
 }
 
 export function usePlan(id) {
-    const [plan, setPlan] = useState(null)
     const { plans, ...rest } = usePlans()
 
-    useEffect(() => {
-        if (plans?.length)
-        {
-            const [ plan ] = plans?.filter(plan => plan.id == id)
-            setPlan(plan)
-        }
-    }, [plans])
-
-    return { ...rest, plan }
+    return { ...rest, plan: plans?.filter(plan => plan.id == id).pop() }
 }
 
 export function useAvailablePaymentMethods()
