9ef4167a613d8fcdbfdc729d299183e3d0b6e7c6
medram <mramouchy@gmail.com>
2023-06-08 18:30:41 +0100

    Password Page created

---
+++ src/pages/account/settings/ChangePasswordPage.jsx
@@ -0,0 +1,10 @@
+
+
+export default function ChangePasswordPage()
+{
+    return (
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae itaque voluptate consectetur facilis, aliquid voluptatum voluptates? Architecto minima at adipisci, nisi similique, consectetur ullam tenetur voluptatem optio, cumque veniam porro.
+        </div>
+    )
+}
