7eb1fdfeef797b78480bfd6eede07a0736262163
medram <mramouchy@gmail.com>
2023-07-05 16:30:44 +0100

    Fixed active chat label

--- src/components/playground/ChatLabel.jsx
+++ src/components/playground/ChatLabel.jsx
@@ -2,10 +2,10 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-export default function ChatLabel({ title, onClick, onDelete })
+export default function ChatLabel({ title, onClick, onDelete, active=false })
 {
     return (
-        <div role="button" className="chat-label" onClick={onClick} >
+        <div role="button" className={["chat-label", active && "active"].join(" ")} onClick={onClick} >
             {title}
             <span className="delete" onClick={onDelete}><FontAwesomeIcon icon={faTrash} /></span>
         </div>
