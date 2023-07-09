ca504df6ace5d85eea5f3cb166ca38087ad6a56c
medram <mramouchy@gmail.com>
2023-07-09 23:46:33 +0100

    Styling fixed

--- src/pages/account/AccountPage.jsx
+++ src/pages/account/AccountPage.jsx
@@ -1,9 +1,7 @@
+import { Navigate } from "react-router-dom"
 
 export default function AccountPage()
 {
-    return (
-        <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque maiores quia voluptates. Deleniti illo autem tempora neque ad atque provident sed temporibus quas natus praesentium expedita est cumque, omnis ut!
-        </div>
-    )
+
+    return <Navigate to="settings" replace={true} />
 }
--- src/pages/layouts/AccountSettingsLayout.jsx
+++ src/pages/layouts/AccountSettingsLayout.jsx
@@ -13,15 +13,15 @@ export default function AccountSettingsLayout() {
         <section className="container py-5">
             <div className="row my-5 gap-5">
                 <div className="col-md-3 col-12">
-                    <div className="list-group">
+                    <div className="list-group rounded">
                         <div className="list-group-item py-4 d-flex justify-content-center align-items-center flex-column" aria-current="true">
                             <AvatarPalceholder size={100} username={user.username} />
                             <b className="my-2">{user.username.slice(0, 1).toUpperCase() + user.username.slice(1)}</b>
                         </div>
-                        <Link to="" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faUser} /> Profile</Link>
-                        <Link to="subscription" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faStar} /> My Subscription</Link>
-                        <Link to="invoices" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faReceipt} /> Invoices</Link>
-                        <Link to="change-password" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faLock} /> Change Password</Link>
+                        <Link to="" className="list-group-item list-group-item-action py-3"><FontAwesomeIcon icon={faUser} /> Profile</Link>
+                        <Link to="subscription" className="list-group-item list-group-item-action py-3"><FontAwesomeIcon icon={faStar} /> My Subscription</Link>
+                        <Link to="invoices" className="list-group-item list-group-item-action py-3"><FontAwesomeIcon icon={faReceipt} /> Invoices</Link>
+                        <Link to="change-password" className="list-group-item list-group-item-action py-3"><FontAwesomeIcon icon={faLock} /> Change Password</Link>
                     </div>
                 </div>
                 <div className="col border rounded p-5 mx-3">
