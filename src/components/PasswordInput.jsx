ad1d20b9e4b2eac90f1ba71882d209aa7eb5f05f
medram <mramouchy@gmail.com>
2023-07-19 20:26:44 +0100

    Hide sensitive date on PasswordInput field

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,15 +1,19 @@
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { memo, useState } from "react";
+import { useDemo } from "../hooks";
 
 
 export default memo(function PasswordInput({ ...restProps })
 {
     const [show, setShow] = useState(false)
+    const { isDemo } = useDemo()
+
+    const value = isDemo ? (restProps?.value ? "**** Hidden for demo mode ****" : "") : restProps?.value
 
     return (
         <div className="input-group mb-3">
-            <input type={show ? "text" : "password" } className="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1" {...restProps} />
+            <input type={show ? "text" : "password" } className="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1" {...restProps} value={value} />
             <span className="input-group-text text-bg-primary" id="basic-addon1" style={{ cursor: "pointer" }} onClick={() => setShow(!show)}>
                 <FontAwesomeIcon icon={show ? faEyeSlash : faEye} />
             </span>
