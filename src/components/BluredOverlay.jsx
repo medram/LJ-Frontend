aed21040a796debdabd6b26066175f059b39e5f2
medram <mramouchy@gmail.com>
2023-07-21 17:03:14 +0100

    BlurOverlay fixed

--- src/components/BluredOverlay.jsx
+++ src/components/BluredOverlay.jsx
@@ -4,12 +4,12 @@ export default function BluredOverlay({ title="", blur=3, children })
 {
     return (
         <div className="blured-overlay">
-            <div style={{
-                filter: `blur(${blur}px)`
-            }}>
+            <div>
                 {children}
             </div>
-
+            <div className="overlay" style={{
+                backdropFilter: `blur(${blur}px)`
+            }}></div>
             <span className="title">{title}</span>
         </div>
     )
