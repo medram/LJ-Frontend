dcf3e8e1006deb53ba4f0bea64000f2e0481a3de
medram <mramouchy@gmail.com>
2023-07-27 22:54:48 +0100

    Fixed bug

--- sync_with_backend.sh
+++ sync_with_backend.sh
@@ -2,12 +2,13 @@
 
 BACKEND_FOLDER="../../laravel/example-app/"
 
-echo "Syncing..."
-find ./build -mindepth 1 -maxdepth 1 ! -name "index.html" -exec cp -r {} "${BACKEND_FOLDER}public/" \;
 
 # Remove previous assets
 echo "Remove old assets..."
 rm -fr ${BACKEND_FOLDER}public/static/*
+
+echo "Syncing..."
+find ./build -mindepth 1 -maxdepth 1 ! -name "index.html" -exec cp -r {} "${BACKEND_FOLDER}public/" \;
 
 echo "Copy index.html..."
 cp build/index.html "${BACKEND_FOLDER}resources/views/frontend.blade.php"
