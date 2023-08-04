e84b0bc10cb32e233fe0c210a3c529efe253edc8
medram <mramouchy@gmail.com>
2023-08-05 00:01:54 +0100

    Fixed bug

--- src/components/playground/AIMessage.jsx
+++ src/components/playground/AIMessage.jsx
@@ -2,9 +2,11 @@
 
 export default function AIMessage({ content })
 {
+    const parsedContent = content.split("\n").map((str, i) => <p key={i}>{str}</p>)
+
     return (
         <div className="message ai-message">
-            {content}
+            {parsedContent}
         </div>
     )
 }
