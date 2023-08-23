f3f61d0fa261ce8d7d68892fd9303d55cfa67393
medram <mramouchy@gmail.com>
2023-08-23 09:23:21 +0100

    Override default react 'jsconfig.json' file.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
 import { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import RegisterPage from "./pages/RegisterPage";
 import { ToastContainer } from "react-toastify"
 
 // loading CSS style
 import 'react-toastify/dist/ReactToastify.css'
-import "./assets/scss/main.scss"
+import "@assets/scss/main.scss"
 
 // loading bootstrap js files.
 import "bootstrap/dist/js/bootstrap"
 
 
-import PricingPage from "./pages/PricingPage";
-import ContactPage from "./pages/ContactPage";
-import AdminDashboardPage from "./pages/admin/AdminDashboardPage";
-import NoAuthRequired from "./pages/middlewares/NoAuthRequired";
-import CustomersPage from "./pages/admin/CustomersPage";
-import AddCustomerPage from "./pages/admin/AddCustomerPage";
-import EditCustomerPage from "./pages/admin/EditCustomerPage";
-import PlansPage from "./pages/admin/PlansPage";
-import { PagesPage } from "./pages/admin/PagesPage";
-import AddPagePage from "./pages/admin/AddPagePage";
-import EditPagePage from "./pages/admin/EditPagePage";
-import { SettingsPage } from "./pages/admin/SettingsPage";
-import PaymentMethodsPage from "./pages/admin/PaymentMethodsPage";
-import CheckoutPage from "./pages/CheckoutPage";
-import { SubscriptionsPage } from "./pages/admin/SubscriptionsPage";
-import UserRequired from "./pages/middlewares/UserRequired";
-import ThankYouPage from "./pages/ThankYouPage";
-import AccountDetailsPage from "./pages/account/settings/AccountDetailsPage";
-import AccountSettingsLayout from "./pages/layouts/AccountSettingsLayout";
-import MySubscriptionPage from "./pages/account/settings/MySubscribtionPage";
-import InvoicesPage from "./pages/account/settings/InvoicesPage";
-import ChangePasswordPage from "./pages/account/settings/ChangePasswordPage";
-import AccountPage from "./pages/account/AccountPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-import PagePage from "./pages/PagePage";
-import LogoutPage from "./pages/LogoutPage";
-import LCPage from "./pages/admin/LCPage";
-import ELRequired from "./pages/middlewares/ELRequired";
+import HomePage from "@pages/HomePage";
+import LoginPage from "@pages/LoginPage";
+import NotFoundPage from "@pages/NotFoundPage";
+import RegisterPage from "@pages/RegisterPage";
+import PricingPage from "@pages/PricingPage";
+import ContactPage from "@pages/ContactPage";
+import AdminDashboardPage from "@pages/admin/AdminDashboardPage";
+import NoAuthRequired from "@pages/middlewares/NoAuthRequired";
+import CustomersPage from "@pages/admin/CustomersPage";
+import AddCustomerPage from "@pages/admin/AddCustomerPage";
+import EditCustomerPage from "@pages/admin/EditCustomerPage";
+import PlansPage from "@pages/admin/PlansPage";
+import { PagesPage } from "@pages/admin/PagesPage";
+import AddPagePage from "@pages/admin/AddPagePage";
+import EditPagePage from "@pages/admin/EditPagePage";
+import { SettingsPage } from "@pages/admin/SettingsPage";
+import PaymentMethodsPage from "@pages/admin/PaymentMethodsPage";
+import CheckoutPage from "@pages/CheckoutPage";
+import { SubscriptionsPage } from "@pages/admin/SubscriptionsPage";
+import UserRequired from "@pages/middlewares/UserRequired";
+import ThankYouPage from "@pages/ThankYouPage";
+import AccountDetailsPage from "@pages/account/settings/AccountDetailsPage";
+import AccountSettingsLayout from "@pages/layouts/AccountSettingsLayout";
+import MySubscriptionPage from "@pages/account/settings/MySubscribtionPage";
+import InvoicesPage from "@pages/account/settings/InvoicesPage";
+import ChangePasswordPage from "@pages/account/settings/ChangePasswordPage";
+import AccountPage from "@pages/account/AccountPage";
+import ResetPasswordPage from "@pages/ResetPasswordPage";
+import PagePage from "@pages/PagePage";
+import LogoutPage from "@pages/LogoutPage";
+import LCPage from "@pages/admin/LCPage";
+import ELRequired from "@pages/middlewares/ELRequired";
 import CookiesAlert from "./components/CookiesAlert";
-import APIKeysPage from "./pages/admin/APIKeysPage";
+import APIKeysPage from "@pages/admin/APIKeysPage";
 
 
 // Lazy loading
-const AdminDashboardLayout = lazy(() => import("./pages/layouts/AdminDashboardLayout"))
-const AccountLayout = lazy(() => import("./pages/layouts/AccountLayout"))
-const PlaygroundPage = lazy(() => import("./pages/PlaygroundPage"))
+const AdminDashboardLayout = lazy(() => import("@pages/layouts/AdminDashboardLayout"))
+const AccountLayout = lazy(() => import("@pages/layouts/AccountLayout"))
+const PlaygroundPage = lazy(() => import("@pages/PlaygroundPage"))
 
 
 function App() {
