6118d106a71a6dd61271650e4824dbaf0aabbb48
medram <mramouchy@gmail.com>
2023-12-01 20:30:01 +0100

    Fixed bug

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -2,6 +2,35 @@ import * as cc from "currency-codes"
 import { toast } from "react-toastify";
 
 
+export const VALID_DOCUMENT_TYPES = [
+    { ext: "pdf",   mimetype: "application/pdf" },
+    { ext: "csv",   mimetype: "text/csv" },
+    { ext: "doc",   mimetype: "application/msword" },
+    { ext: "docx",  mimetype: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
+    { ext: "xls",   mimetype: "application/vnd.ms-excel" },
+    { ext: "xlsx",  mimetype: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
+    { ext: "txt",   mimetype: "text/plain" },
+    { ext: "json",  mimetype: "application/json" },
+    { ext: "ppt",   mimetype: "application/vnd.ms-powerpoint" },
+    { ext: "pptx",  mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation" },
+    { ext: "epub",  mimetype: "application/epub+zip" },
+]
+
+export function getAvailableDocumentTypes()
+{
+    const types = {}
+    VALID_DOCUMENT_TYPES.map((type) => {
+        types[type.mimetype] = [`.${type.ext}`]
+    })
+
+    return types
+}
+
+export function getAvailableDocumentTypesString()
+{
+    return VALID_DOCUMENT_TYPES.map(type => type.ext).join(", ")
+}
+
 export function toastFormikErrors(errors)
 {
     if (Object.keys(errors).length)
@@ -39,3 +68,4 @@ export function getRandomItem(list) {
     const randomIndex = Math.floor(Math.random() * list.length);
     return list[randomIndex];
 }
+
