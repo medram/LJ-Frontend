5f4832c06b81417e9e1202d2e742312ed674bfa8
medram <mramouchy@gmail.com>
2023-12-03 19:43:28 +0100

    AI message improved

--- src/components/playground/AIMessage.jsx
+++ src/components/playground/AIMessage.jsx
@@ -1,14 +1,16 @@
 import AIAvatar from "@images/playground/AI-avatar.png"
+import Markdown from 'react-markdown'
+
 
 export default function AIMessage({ content })
 {
-    const parsedContent = content.split("\n").map((str, i) => <p key={i}>{str}</p>)
+    // const parsedContent = content.split("\n").map((str, i) => <p key={i}>{str}</p>)
 
     return (
         <div className="message ai-message">
             <img src={AIAvatar} className="avatar" />
 
-            {parsedContent}
+            <Markdown>{content}</Markdown>
         </div>
     )
 }
