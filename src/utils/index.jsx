51605c05fd68f1404938c9e5bf5468c88c95e7f1
medram <mramouchy@gmail.com>
2023-12-05 18:25:53 +0100

    AI models added

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -2,12 +2,22 @@ import * as cc from "currency-codes"
 import { toast } from "react-toastify";
 
 
+export const AVAILABLE_AI_MODELS = [
+    { label: "gpt-3.5-turbo",           value: "gpt-3.5-turbo" },
+    { label: "gpt-3.5-turbo-0301",      value: "gpt-3.5-turbo-0301" },
+    { label: "gpt-3.5-turbo-0613",      value: "gpt-3.5-turbo-0613" },
+    { label: "gpt-3.5-turbo-1106",      value: "gpt-3.5-turbo-1106" },
+    { label: "gpt-3.5-turbo-16k",       value: "gpt-3.5-turbo-16k" },
+    { label: "gpt-3.5-turbo-16k-0613",  value: "gpt-3.5-turbo-16k-0613" },
+    { label: "gpt-4",                   value: "gpt-4" },
+    { label: "gpt-4-0314",              value: "gpt-4-0314" },
+    { label: "gpt-4-0613",              value: "gpt-4-0613" },
+    { label: "gpt-4-1106-preview",      value: "gpt-4-1106-preview" },
+]
+
 export const VALID_DOCUMENT_TYPES = [
     { ext: "pdf",   mimetype: "application/pdf" },
     { ext: "csv",   mimetype: "text/csv" },
-    // { ext: "doc",   mimetype: "application/msword" },
-    // { ext: "xls",   mimetype: "application/vnd.ms-excel" },
-    // { ext: "ppt",   mimetype: "application/vnd.ms-powerpoint" },
     { ext: "docx",  mimetype: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
     { ext: "xlsx",  mimetype: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
     { ext: "txt",   mimetype: "text/plain" },
