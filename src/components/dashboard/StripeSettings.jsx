e04ef224caaae7d3c3e97e77d822684861dff3d5
medram <mramouchy@gmail.com>
2024-02-06 14:57:56 +0100

    Enable Stripe settings UI

--- src/components/dashboard/StripeSettings.jsx
+++ src/components/dashboard/StripeSettings.jsx
@@ -1,14 +1,15 @@
-import { toast } from "react-toastify";
-import { useQueryClient } from "react-query";
 import { useFormik } from "formik";
 import { memo } from "react";
-import * as Yup from "yup"
+import { useQueryClient } from "react-query";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
 
-import { saveDashboardSettings } from "../../api/admin";
+import SuperButton from "@components/SuperButton";
+import { saveDashboardSettings, syncWithStripe } from "../../api/admin";
+import { useDemo } from "../../hooks";
 import { useModel } from "../../hooks/templates";
 import PasswordInput from "../PasswordInput";
 import Switch from "../Switch";
-import { useDemo } from "../../hooks";
 import StripIcon from "../icons/StripIcon";
 import GatewayNotes from "./GatewayNotes";
 
@@ -21,14 +22,14 @@ export default memo(function StripeSettings({ settings }) {
 
     const formik = useFormik({
         initialValues: {
-            PM_STRIP_PUBLIC_KEY: settings?.PM_STRIP_PUBLIC_KEY,
-            PM_STRIP_PRIVATE_KEY: settings?.PM_STRIP_PRIVATE_KEY,
+            PM_STRIP_SECRET_KEY: settings?.PM_STRIP_SECRET_KEY,
+            PM_STRIP_SECRET_KEY_TEST: settings?.PM_STRIP_SECRET_KEY_TEST,
             PM_STRIP_SANDBOX: settings?.PM_STRIP_SANDBOX,
             PM_STRIP_STATUS: settings?.PM_STRIP_STATUS,
         },
         validationSchema: Yup.object({
-            PM_STRIP_PUBLIC_KEY: Yup.string(),
-            PM_STRIP_PRIVATE_KEY: Yup.string(),
+            PM_STRIP_SECRET_KEY: Yup.string(),
+            PM_STRIP_SECRET_KEY_TEST: Yup.string(),
             PM_STRIP_SANDBOX: Yup.boolean(),
             PM_STRIP_STATUS: Yup.boolean(),
         }),
@@ -37,8 +38,8 @@ export default memo(function StripeSettings({ settings }) {
             if (isDemo)
                 return toast.success("This action isn't allowed on the demo mode!")
 
-            if (values.PM_STRIP_STATUS && (!values.PM_STRIP_PUBLIC_KEY.length || !values.PM_STRIP_PRIVATE_KEY.length)) {
-                toast.warning("Please fill up Stripe Client ID and Secret fields first before activation!")
+            if (values.PM_STRIP_STATUS && (!values.PM_STRIP_SECRET_KEY_TEST.length || !values.PM_STRIP_SECRET_KEY.length)) {
+                toast.warning("Please fill up Stripe Secret Keys fields first before activation!")
                 formik.setSubmitting(false)
             }
             else {
@@ -55,19 +56,20 @@ export default memo(function StripeSettings({ settings }) {
                     // update Stripe Webhook
 
                     if (
-                        values.PM_STRIP_PUBLIC_KEY !== settings.PM_STRIP_PUBLIC_KEY
-                        || values.PM_STRIP_PRIVATE_KEY !== settings.PM_STRIP_PRIVATE_KEY) {
-                        // registerStripeWebhook().then((req) => {
-                        //     if (req.status === 201 && !req.data?.errors) {
-                        //         toast.success(req.data?.message)
-                        //     }
-                        // }).catch(err => {
-                        //     if (err.response.status === 400) {
-                        //         toast.error(err.response.data?.message)
-                        //     }
-                        // }).finally(() => {
-                        //     formik.setSubmitting(false)
-                        // })
+                        values.PM_STRIP_SECRET_KEY_TEST !== settings.PM_STRIP_SECRET_KEY_TEST
+                        || values.PM_STRIP_SECRET_KEY !== settings.PM_STRIP_SECRET_KEY) {
+
+                        syncWithStripe(values).then((req) => {
+                            if (req.status === 201 && !req.data?.errors) {
+                                toast.success(req.data?.message)
+                            }
+                        }).catch(err => {
+                            if (err.response.status === 400) {
+                                toast.error(err.response.data?.message)
+                            }
+                        }).finally(() => {
+                            formik.setSubmitting(false)
+                        })
                     }
                     else {
                         formik.setSubmitting(false)
@@ -98,29 +100,33 @@ export default memo(function StripeSettings({ settings }) {
             </div>
 
             <Model title="Stripe Gateway Settings" footer={
-                <button className="btn btn-primary w-25">Save</button>
+                <SuperButton className="btn btn-primary w-25" onClick={() => formik.submitForm()} isLoading={formik.isSubmitting}>
+                    Save
+                </SuperButton>
             }>
-                <div className="mb-4">
-                    <label htmlFor="client-id">Public Key:</label>
-                    <input type="text" className="form-control" id="client-id" {...formik.getFieldProps("PM_STRIP_PUBLIC_KEY")} />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="private-key">Private Key:</label>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="mb-4">
+                        <label htmlFor="secret-key">Secret Key (live):</label>
 
-                    <PasswordInput id="private-key" placeholder="" {...formik.getFieldProps("PM_STRIP_PRIVATE_KEY")} />
-                </div>
+                        <PasswordInput id="secret-key" placeholder="sk_live_xxxxxxxxxx" {...formik.getFieldProps("PM_STRIP_SECRET_KEY")} />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="secret-key-test">Secret Key (test):</label>
+                        <PasswordInput id="secret-key-test" placeholder="sk_test_xxxxxxxxxx" {...formik.getFieldProps("PM_STRIP_SECRET_KEY_TEST")} />
+                    </div>
 
-                <div className="d-flex">
-                    <Switch onChange={(checked) => formik.setFieldValue("PM_STRIP_STATUS", checked)} name="accept" checked={formik.values.PM_STRIP_STATUS} size="small" className="mx-2 mt-1" />
+                    <div className="d-flex">
+                        <Switch onChange={(checked) => formik.setFieldValue("PM_STRIP_STATUS", checked)} name="accept" checked={formik.values.PM_STRIP_STATUS} size="small" className="mx-2 mt-1" />
 
-                    <label htmlFor="status" className="form-label" onClick={() => formik.setFieldValue("PM_STRIP_STATUS", !formik.values.PM_STRIP_STATUS)} >{formik.values.PM_STRIP_STATUS ? "Active" : "Inactive"}</label>
-                </div>
+                        <label htmlFor="status" className="form-label" onClick={() => formik.setFieldValue("PM_STRIP_STATUS", !formik.values.PM_STRIP_STATUS)} >{formik.values.PM_STRIP_STATUS ? "Active" : "Inactive"}</label>
+                    </div>
 
-                <div className="d-flex">
-                    <Switch onChange={(checked) => formik.setFieldValue("PM_STRIP_SANDBOX", checked)} name="accept" checked={formik.values.PM_STRIP_SANDBOX} size="small" className="mx-2 mt-1" />
+                    <div className="d-flex">
+                        <Switch onChange={(checked) => formik.setFieldValue("PM_STRIP_SANDBOX", checked)} name="accept" checked={formik.values.PM_STRIP_SANDBOX} size="small" className="mx-2 mt-1" />
 
-                    <label htmlFor="PM_STRIP_SANDBOX" className="form-label" onClick={() => formik.setFieldValue("PM_STRIP_SANDBOX", !formik.values.PM_STRIP_SANDBOX)} >Sandbox <i><small>(test mode)</small></i></label>
-                </div>
+                        <label htmlFor="PM_STRIP_SANDBOX" className="form-label" onClick={() => formik.setFieldValue("PM_STRIP_SANDBOX", !formik.values.PM_STRIP_SANDBOX)} >Sandbox <i><small>(test mode)</small></i></label>
+                    </div>
+                </form>
 
                 <GatewayNotes />
             </Model>
