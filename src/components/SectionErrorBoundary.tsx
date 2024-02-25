128ce7f6c33dc487466b5ef479fb95014049c805
medram <mramouchy@gmail.com>
2024-02-25 14:56:18 +0100

    Section error boudary component added

---
+++ src/components/SectionErrorBoundary.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import SectionFallback from "./errors/SectionFallback";
+
+
+export default function SectionErrorBoundary({ children }: { children: ReactNode })
+{
+	return <ErrorBoundary FallbackComponent={SectionFallback}>
+		{children}
+	</ErrorBoundary>
+}
